70a476dafc83caff02a72c5272631ae84cc6734b
huycuuvan <huynd130904@gmail.com>
2024-08-10 16:13:46 +0700

    updated newest

--- StudentManager/obj/Debug/net8.0/StudentManager.AssemblyInfo.cs
+++ StudentManager/obj/Debug/net8.0/StudentManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e63e2430e939d7969e13ed21157a6f5757b89e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38dc2c1ceeffe5ee6273a2ca45e6377ac48c5d99")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
